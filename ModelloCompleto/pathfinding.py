6fb62981dcd4f062e2e8c0dad9b9d2a7b8894909
fedetabu <152106494+fedetabu@users.noreply.github.com>
2026-05-22 00:16:07 +0200

    Update pathfinding.py
    implementati ghiaccio e aeroporti

--- ModelloCompleto/pathfinding.py
+++ ModelloCompleto/pathfinding.py
@@ -17,15 +17,34 @@ class MappaProblem(Problem):
         self.costo_base= costo_base
         self.fattore_ostile= fattore_ostile
 
+        self.aeroporti = {}    # Creo dizionario dove salvare per ogni territorio F il suo "Gate" (l'unico pixel del territorio da cui e verso cui è permesso il volo)
+        for y in range(self.max_y):
+            for x in range(self.max_x):
+                valore_casella = str(self.matrice[y, x, 1])  # Lo leggo come stringa per poter usare startswith
+                if valore_casella.startswith("F"):
+                    if valore_casella not in self.aeroporti:  # Se non ho ancora salvato un Gate per questo aeroporto  
+                        self.aeroporti[valore_casella] = (x, y) # Salvo questo pixel come suo Gate Ufficiale! (sarà il pixel più a Nord-Ovest di ogni territorio F)
+
 
     def actions(self, state):
         """
         Dato uno stato (X, Y), restituisce le mosse valide.
         """
-        self.nodi_esplorati += 1
+        self.nodi_esplorati += 1 
 
         x, y = state
         mosse_valide = []
+
+        # --- LOGICA VOLI AEROPORTO ---
+        valore_corrente = str(self.matrice[y, x, 1])
+        
+        if valore_corrente.startswith("F"):
+            # Scorro il dizionario dei Gate ufficiali (.items() tira fuori chiave e valore)
+            for id_destinazione, (nx, ny) in self.aeroporti.items():
+                # Se il Gate di destinazione è di un aeroporto diverso da quello attuale
+                if id_destinazione != valore_corrente:
+                    mosse_valide.append((nx, ny))
+        # -----------------------------------
 
         direzioni = [
              (0, -1), # Nord
@@ -48,8 +67,8 @@ class MappaProblem(Problem):
                          nx += d[0]
                          if 0 <= nx < self.max_x and 0 <= ny < self.max_y: #controllo che non vada fuori dalla mappa
                              valore_pixel = self.matrice[ny, nx, 1]
-                         else:
-                             schianto = True #altrimenti vuol dire che si è schiantato con un bordo della mappa
+                         else:      #altrimenti vuol dire che si è schiantato con un bordo della mappa
+                             schianto = True 
                              break
                      if valore_pixel == "X" or valore_pixel == "VUOTO" or schianto:
                          mosse_valide.append((nx-d[0], ny-d[1])) #annullo l'ultima mossa se finisco in una x, vuoto o mi sono schiantato (impossibile perché prima ci sarebbe il confine)
@@ -67,22 +86,33 @@ class MappaProblem(Problem):
         """
         Calcola il costo totale per arrivare da state1 a state2.
         """
-        x, y = state2
+        x, y = state2 #coordinate nodo arrivo
+        px, py = state1 #coordinate nodo partenza
 
-        valore_pixel = self.matrice[y, x, 1]
+        # Leggo come stringhe per poter usare .startswith()
+        valore_arrivo = str(self.matrice[y, x, 1])
+        valore_partenza = str(self.matrice[py, px, 1])
+       
+        if valore_partenza.startswith("F") and valore_arrivo.startswith("F") and valore_partenza != valore_arrivo: # valore_partenza!=valore_arrivo perché sennò entrerebbe anche per gli spostamenti dentro F
+            # È un volo, non si considera la distanza, il costo è fisso (il "biglietto")
+            costo_movimento = self.costo_base * 3 # ho messo che il "biglietto" del volo costa 3 volte lo spostamento base (vedremo se modificarlo o anche richiederlo eventualmente)
+        else:
+            # È un passo normale (distanza = 1) o una scivolata su ghiaccio (distanza > 1)
+            distanza = abs(x - px) + abs(y - py)
+            costo_movimento = distanza * self.costo_base
 
-        costo_aggiuntivo=0
+        costo_aggiuntivo=0  # Eventuali costi aggiuntivi della cella di arrivo
 
-        if valore_pixel.isdigit():
-            costo_aggiuntivo = int(valore_pixel)* self.fattore_ostile
+        if valore_arrivo.isdigit():
+            costo_aggiuntivo = int(valore_arrivo)* self.fattore_ostile
 
-        elif valore_pixel == "X":
+        elif valore_arrivo == "X":   # Non necessario in quanto già viene evitato per definizione su actions
             costo_aggiuntivo = 999999
 
-        elif valore_pixel == "CONFINE":
+        elif valore_arrivo == "CONFINE":
             costo_aggiuntivo = self.costo_confine
-
-        return c + costo_aggiuntivo + self.costo_base
+        
+        return c + costo_aggiuntivo + costo_movimento
 
     def h1(self, node):
         """
